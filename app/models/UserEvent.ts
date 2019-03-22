8278c7d1102bf8edfadd6c2b513b9f75f0d9d4f0
andrew.scala <agscala@gmail.com>
2019-03-22 17:03:03 -0400

    UserEvent pre-save hooks for setting created timestamp

--- app/models/UserEvent.ts
+++ app/models/UserEvent.ts
@@ -1,8 +1,9 @@
 import { model, Document, Schema } from 'mongoose';
 
 export interface IUserEvent {
-    name: string;
-    password: string;
+    type: string;
+    user: Schema.Types.ObjectId;
+    created?: Date;
 }
 
 export interface IUserEventDocument extends IUserEvent, Document {
@@ -19,6 +20,11 @@ export const UserEventSchema = new Schema({
     type: String,
     created: Date,
     user: {type: Schema.Types.ObjectId, ref: 'User'},
+});
+
+UserEventSchema.pre("save", function (this: IUserEventDocument, next) {
+    this.created = new Date();
+    next();
 });
 
 export const UserEvent = model<IUserEventDocument>('UserEvent', UserEventSchema);
